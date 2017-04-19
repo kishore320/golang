f6c6ddccd8bdd0ea450a2764ab7923a027c552ea
Nagakishore Sidde <nagakishore.sidde@oracle.com>
2017-04-19 19:42:52 +0530

    Modifications to setUsingMap.go

--- src/datastructures/setUsingMap.go
+++ src/datastructures/setUsingMap.go
@@ -1,4 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+	sampleSet := make(Set)
+	sampleSet["key1"] = struct{}{}
+	sampleSet["key2"] = struct{}{}
+	// There will be only one key2 in the set
+	sampleSet["key2"] = struct{}{}
+
+	fmt.Println("Set contents : ", getSetValues(sampleSet))
+}
+
+type Set map[string]struct{}
+
+func getSetValues(s Set) []string {
+	var resArr []string
+	for key, _ := range s {
+		resArr = append(resArr, key)
+	}
+
+	return resArr
 }
