ac259095f08111971a293ff118e683485a4e94e1
Nagakishore Sidde <nagakishore.sidde@oracle.com>
2017-04-20 14:05:40 +0530

    Added interesting GoRoutineWithAnonymous function which demonstrates how goroutines are executed with in the same address space as caller

--- src/concurrency/goroutineDemo.go
+++ src/concurrency/goroutineDemo.go
@@ -10,6 +10,8 @@ func main() {
 	withoutGoRoutine()
 	fmt.Println()
 	withGoRoutine()
+	fmt.Println()
+	withGoRoutineAnonymousFunction()
 }
 
 // Should print " world! Hello "
@@ -23,6 +25,21 @@ func withoutGoRoutine() {
 func withGoRoutine() {
 	go printMsg("world!")
 	fmt.Print(" Hello ")
+	time.Sleep(time.Millisecond * 300)
+}
+
+// Goroutines are executed within the same address space of the caller
+// Interesting... This will still print " Hello world! "
+func withGoRoutineAnonymousFunction() {
+	word := "Hello"
+	go func() {
+		time.Sleep(time.Millisecond * 100)
+		// word value will no longer be "Hello", rather it will be " world! "
+		// because, its value was changed outside of this anonymous function below (i.e) after fmt.Print(" Hello ") statement
+		fmt.Print(word)
+	}()
+	fmt.Print(" Hello ")
+	word = " world! "
 	time.Sleep(time.Millisecond * 300)
 }
 
